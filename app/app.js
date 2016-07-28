17f2e36da98a0133814c0514ded74b001ce8fdb3
YAN7 <904394373@qq.com>
2016-07-28 18:02:46 +0800

    完成搜索模块，但加上搜索模块之后不管点击第几页都是显示的第一页的内容，现在尝试修复

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 * @Author: yan7
 * @Date:   2016-07-24 15:24:25
 * @Last Modified by:   yan7
-* @Last Modified time: 2016-07-27 20:34:06
+* @Last Modified time: 2016-07-28 17:48:10
 */
 
 (function (angular) {
@@ -16,5 +16,16 @@
     	// 'coming_soon',
     	// 'top250'
     	]);
+
+      // 创建控制器
+      app.controller('mainController', ['$scope', '$location', function($scope, $location) {
+        $scope.query = '';
+        $scope.search = function () {
+          // 穿入一个字符串参数，就是用来改变字符串的锚点值
+          $location.url('/search?q=' + $scope.query);
+        }
+      }]);
+
+
 
 })(angular);
---
+++ app/search/search.js
@@ -0,0 +1,8 @@
+/*
+* @Author: yan7
+* @Date:   2016-07-28 18:01:16
+* @Last Modified by:   yan7
+* @Last Modified time: 2016-07-28 18:01:26
+*/
+
+()();
